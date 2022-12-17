0ca394f00cd97974c220cd7306749620a35a817c
mrlthebest <115442027+mrlthebest@users.noreply.github.com>
2022-12-16 22:47:26 -0300

    Update potion.lua

--- potion.lua
+++ potion.lua
@@ -8,17 +8,17 @@ potionSetup = storage.potion:split(',')
 
 macro(100, "Potion", function()
     for _, potionConfig in ipairs(potionSetup) do
-        if (hppercent() <= tonumber(potionConfig[2]) or manapercent() <= tonumber(potionConfig[3])) and
+        if (hppercent() <= tonumber(potionSetup[2]) or manapercent() <= tonumber(potionSetup[3])) and
             (not potionCD or potionCD <= now) then
-                return useWith(tonumber(potionConfig[1]), player)
+                return useWith(tonumber(potionSetup[1]), player)
             end
         end
 end)
 
 onUseWith(function(sla, itemId)
     for _, potionConfig in ipairs(potionSetup) do
-        if tonumber(potionConfig[1]) == itemId then
-            potionCD = now + tonumber(potionConfig[4])
+        if tonumber(potionSetup[1]) == itemId then
+            potionCD = now + tonumber(potionSetup[4])
         end
     end
 end)
