e01204cd6106838fb44b74c539e19eb3ded0cc29
mrlthebest <115442027+mrlthebest@users.noreply.github.com>
2022-12-16 22:48:58 -0300

    Update potion.lua

--- potion.lua
+++ potion.lua
@@ -3,22 +3,19 @@ addTextEdit("Config Potion", storage.potion or "ID, HP, MP, CD", function(widget
 end)
 
 
-potionSetup = storage.potion:split(',')
+potionSetup = tonumber(storage.potion:split(','))
 
 
 macro(100, "Potion", function()
-    for _, potionConfig in ipairs(potionSetup) do
-        if (hppercent() <= tonumber(potionSetup[2]) or manapercent() <= tonumber(potionSetup[3])) and
+        if (hppercent() <= potionSetup[2] or manapercent() <= potionSetup[3] and
             (not potionCD or potionCD <= now) then
-                return useWith(tonumber(potionSetup[1]), player)
-            end
+            return useWith(potionSetup[1], player)
         end
 end)
 
 onUseWith(function(sla, itemId)
-    for _, potionConfig in ipairs(potionSetup) do
-        if tonumber(potionSetup[1]) == itemId then
-            potionCD = now + tonumber(potionSetup[4])
-        end
+        if potionSetup[1] == itemId then
+            potionCD = now + potionSetup[4]
     end
 end)
+
