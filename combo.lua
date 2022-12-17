58d0601f7a37dc86e51401f0e4b63cb3724b09d4
mrlthebest <115442027+mrlthebest@users.noreply.github.com>
2022-12-16 22:35:17 -0300

    Update combo.lua

--- combo.lua
+++ combo.lua
@@ -1,34 +1,41 @@
-addTextEdit("Magias", storage.ComboText or "Magias", function(widget, text) 
-    storage.ComboText = text
+addTextEdit("Magias", storage.comboSPELL or "MAGIA1, MAGIA2, MAGIA3", function(widget, text)
+    storage.comboSPELL = text
+end)
+comboSpells = strorage.comboSPELL:split(','):lower()
+
+
+addTextEdit("CD", storage.comboCD or "CD1, CD2, CD3", function(widget, text)
+    storage.comboCD = text
+end)
+comboCEDE = tonumber(strorage.comboCD:split(','))
+
+
+addTextEdit("Dist", storage.comboDISTANCIA or "DIST1, DIST2, DIST3", function(widget, text)
+    storage.comboDISTANCIA = text
+end)
+comboDIST = tonumber(strorage.comboDISTANCIA:split(','))
+
+    
+macro(100, 'combo', function()
+    local target = g_game.getAttackingCreature()
+        if not g_game.isAttacking() then return end
+          if target then   
+            targetPos = target:getPosition()
+            if targetPos then
+                distanceToTarget = getDistanceBetween(pos(), targetPos)
+                if distanteToTarget <= comboDIST and (not comboCD or comboCD < now) then
+                    say(comboSpells)
+                end
+            end
+        end
+    end
 end)
 
-
-addTextEdit("Tempo", storage.ComboTempo or "Tempo", function(widget, text) 
-    storage.ComboTempo = text
-end)
-
-local combo = storage.ComboText:split(',')
-local tempo = tonumber(storage.ComboTempo:split(','))
 
 onTalk(function(name, level, mode, text)
     if name ~= player:getName() then return end
-
-    text = text:lower()
-    if text == combo then
-       storage.ComcoCD = os.time() + tempo
+                        
+text = text:lower()
+    if text == comboSpells then
+         comboCD = now + comboCEDE
     end
-end)
-
-if (not storage.ComboCD) then
-    storage.ComboCD= 0;
-  end
-
-
-macro(100, "Combo", function()
-    if not g_game.isAttacking() then return end
-        for _, spell in ipairs(combo) do
-            if storage.ComboCD <= os.time() then
-         say(spell)
-    end
-   end
-end)
