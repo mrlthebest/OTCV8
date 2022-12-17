d96318cfa4f7bb4dd4c9c6988817880a917b979d
mrlthebest <115442027+mrlthebest@users.noreply.github.com>
2022-12-16 22:37:19 -0300

    Update fuga.lua

--- fuga.lua
+++ fuga.lua
@@ -3,31 +3,20 @@ addTextEdit("Spells", storage.fugaSPELL or "SPELL1, SPELL2, SPELL 3", function(w
 end)
 fugaSpells = strorage.fugaSPELL:split(','):lower()
 
-
-
 addTextEdit("Hp", storage.fugaHP or "HP1, HP2, HP3", function(widget, text)
     storage.fugaHP = text
 end)
 fugaHP = tonumber(strorage.fugaHP:split(','))
-
-
 
 addTextEdit("CD", storage.fugaCD or "CD1, CD2, CD3", function(widget, text)
     storage.fugaCD = text
 end)
 fugaCEDE = tonumber(strorage.fugaCD:split(','))
 
-
-
-
 addTextEdit("Distance", storage.fugaDISTANCIA or "DIST1, DIST2, DIST3", function(widget, text)
     storage.fugaDISTANCIA = text
 end)
 fugaDIST = tonumber(strorage.fugaDISTANCIA:split(','))
-
-
-local colorToMatch = {r = 0, g = 0, b = 0, a = 255}
-spec:isTimedSquareVisible() and table.equals(spec:getTimedSquareColor(), colorToMatch)
 
 macro(100, "Fuga", function()
     for _, spec in ipairs(getSpectators()) do
