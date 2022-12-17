b19f8db234f53f7d043b6a781be284b01dd9f39d
mrlthebest <115442027+mrlthebest@users.noreply.github.com>
2022-12-16 22:42:03 -0300

    Create potion.lua

---
+++ potion.lua
@@ -0,0 +1,24 @@
+addTextEdit("Config Potion", storage.potion or "ID, HP, MP, CD", function(widget, text)
+    storage.potion = text
+end)
+
+
+potionSetup = storage.potion:split(',')
+
+
+macro(100, "Potion", function()
+    for _, potionConfig in ipairs(potionSetup) do
+        if (hppercent() <= tonumber(potionConfig[2]) or manapercent() <= tonumber(potionConfig[3])) and
+            (not potionCD or potionCd <= now) then
+                return useWith(tonumber(potionConfig[1]), player)
+            end
+        end
+end)
+
+onUseWith(function(sla, itemId)
+    for _, potionConfig in ipairs(potionSetup) do
+        if tonumber(potionConfig[1]) == itemId then
+            potionCD = now + tonumber(potionConfig[4])
+        end
+    end
+end)
