d03eeccb1d0471467aa3e09eb71006ba39a32347
mrlthebest <115442027+mrlthebest@users.noreply.github.com>
2022-12-18 01:23:19 -0300

    Update potion.lua

--- potion.lua
+++ potion.lua
@@ -1,21 +1,22 @@
 addTextEdit("Config Potion", storage.potion or "ID, HP, MP, CD", function(widget, text)
+    if text and #text:split(",") < 4 then
+        return warn("por favor, inserir os valores na ordem (ID, HP, MP, CD)")
+    end
     storage.potion = text
 end)
 
-
-potionSetup = tonumber(storage.potion:split(','))
-
-
 macro(100, "Potion", function()
-        if (hppercent() <= potionSetup[2] or manapercent() <= potionSetup[3] and
-            (not potionCD or potionCD <= now) then
-            return useWith(potionSetup[1], player)
-        end
-end)
+    local split = storage.potion:split(",")
+    local id, hp, mp = tonumber(split[1]), tonumber(split[2]), tonumber(split[3])
+    if (hppercent() <= hp or manapercent() <= mp) and (not potionCD or potionCD <= now) then
+         useWith(id, player)
+    end
+end)  
 
-onUseWith(function(sla, itemId)
-        if potionSetup[1] == itemId then
-            potionCD = now + potionSetup[4]
+onUseWith(function(_, itemId)
+    if itemId == tonumber(storage.potion:split(",")[1]) then
+        potionCD = storage.potion:split(",")[4] + now
     end
 end)
+
 
