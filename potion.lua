7aa51932958a73355635bbf2873184e0094209b6
mrlthebest <115442027+mrlthebest@users.noreply.github.com>
2023-07-27 20:50:51 -0300

    Update potion.lua

--- potion.lua
+++ potion.lua
@@ -1,22 +1,22 @@
+local s = {}
+
+macro(100, "Potion", function()
+    local configSenzu = tonumber(storage.potion:split(","));
+    if (hppercent() <= configSenzu[2] or manapercent() <= configSenzu[3]) and (not s.cdW or s.cdW <= now) then
+        useWith(configSenzu[1], player)
+    end
+end)
+
+onUseWith(function(pos, itemId, target, subType)
+    local configSenzu = tonumber(storage.potion:split(","));
+    if itemId == configSenzu[1] then
+        s.cdW = now + configSenzu[4]
+    end
+end)
+
 addTextEdit("Config Potion", storage.potion or "ID, HP, MP, CD", function(widget, text)
     if text and #text:split(",") < 4 then
         return warn("por favor, inserir os valores na ordem (ID, HP, MP, CD)")
     end
     storage.potion = text
 end)
-
-macro(100, "Potion", function()
-    local split = storage.potion:split(",")
-    local id, hp, mp = tonumber(split[1]), tonumber(split[2]), tonumber(split[3])
-    if (hppercent() <= hp or manapercent() <= mp) and (not potionCD or potionCD <= now) then
-         useWith(id, player)
-    end
-end)  
-
-onUseWith(function(_, itemId)
-    if itemId == tonumber(storage.potion:split(",")[1]) then
-        potionCD = storage.potion:split(",")[4] + now
-    end
-end)
-
-
