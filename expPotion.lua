c314ce0adbcfd015de869c1e3442adf5e8e6a1cd
mrlthebest <115442027+mrlthebest@users.noreply.github.com>
2023-07-28 20:56:43 -0300

    expPotion.lua

--- expPotion.lua
+++ expPotion.lua
@@ -8,10 +8,11 @@ local p = {}
 
 macro(100, "Exp Potion", function()
     local setup = storage.expSetup:split(",");
+        local potId = tonumber(setup[1])
     if isInPz() then return; end
     if not p.cdW or p.cdW <= os.time() then
-        if findItem(setup[1]) then
-            useWith(setup[1], player)
+        if findItem(potId) then
+            useWith(potId, player)
         else
             warn('Exp pot não encontrada.')
             return
@@ -21,8 +22,9 @@ end)
 
 onUseWith(function(pos, itemId, target, subType)
     local setup = storage.expSetup:split(",");
-    if itemId == setup[1] then
-        p.cdW = os.time() + (setup[2] * 60)
+    local potId = tonumber(setup[1])
+    if itemId == potId then
+        p.cdW = os.time() + (tonumber(setup[2]) * 60)
     end
 end)
 
@@ -31,6 +33,6 @@ addTextEdit("ID, MINUTOS", storage.expSetup or "ID, MINUTOS", function(widget, t
     if text and #text:split(",") < 2 then
         return warn("por favor, inserir os valores na ordem (ID, MINUTOS)")
     end
-    storage.expSetup = tonumber(text);
+    storage.expSetup = text
 end)
 
