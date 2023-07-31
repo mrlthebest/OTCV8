9dc74f2a08a2b9402a015dd3de3de4c849ef761c
mrlthebest <115442027+mrlthebest@users.noreply.github.com>
2023-07-30 23:47:30 -0300

    potion.lua

--- potion.lua
+++ potion.lua
@@ -4,25 +4,25 @@
     28/07/2023
 ]]--
 
-local s = {}
+local s = {};
 
 macro(100, "Potion", function()
-    local configSenzu = storage.potion:split(",");
-    if (hppercent() <= tonumber(configSenzu[2]) or manapercent() <= tonumber(configSenzu[3])) and (not s.cdW or s.cdW <= now) then
-        useWith(tonumber(configSenzu[1]), player)
+    local configPotion = storage.potion:split(",");
+    if (hppercent() <= tonumber(configPotion[2]) or manapercent() <= tonumber(configPotion[3])) and (not s.cdW or s.cdW <= now) then
+        useWith(tonumber(configPotion[1]), player);
     end
-end)
+end);
 
 onUseWith(function(pos, itemId, target, subType)
-    local configSenzu = storage.potion:split(",");
-    if itemId == tonumber(configSenzu[1]) then
-        s.cdW = now + tonumber(configSenzu[4])
+    local configPotion = storage.potion:split(",");
+    if itemId == tonumber(configPotion[1]) then
+        s.cdW = now + tonumber(configPotion[4]);
     end
-end)
+end);
 
 addTextEdit("Config Potion", storage.potion or "ID, HP, MP, CD", function(widget, text)
     if text and #text:split(",") < 4 then
-        return warn("por favor, inserir os valores na ordem (ID, HP, MP, CD)")
+        return warn("por favor, inserir os valores na ordem (ID, HP, MP, CD)");
     end
-    storage.potion = text
-end)
+    storage.potion = text;
+end);
