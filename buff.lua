1bdb3fd88ca53e8d28c8c6c797c77d8b20a96e08
mrlthebest <115442027+mrlthebest@users.noreply.github.com>
2023-09-20 01:59:00 -0300

    buff.lua

--- buff.lua
+++ buff.lua
@@ -1,5 +1,10 @@
+local checkBox = {}
+
 UI.Label('Buff: buff spell, buff orange message, cd');
-local buffConfig = {}
+
+if not storage.buffCDW then
+    storage.buffCDW = 0;
+end
 
 addTextEdit("Buff ", storage.buffConfig or "Buff", function(widget, text)
     storage.buffConfig = text:trim():lower()
@@ -9,7 +14,7 @@ end)
 macro(100, "Buff", function()
     local buffSetup = storage.buffConfig:split(',');
     if isInPz() then return; end
-    if (not buffConfig.cdW or buffConfig.cdW <= os.time()) then
+    if storage.buffCDW<= os.time() and (not storage.castPZ) or (storage.castPZ and not isInPz()) then
         say(buffSetup[1])
     end
 end)
@@ -19,6 +24,21 @@ onTalk(function(name, level, mode, text, channelId, pos)
     local buffSetup = storage.buffConfig:split(',');
     local textBuff = buffSetup[2] and buffSetup[2]:trim() or buffSetup[1]:trim()
     if text:lower():trim() == textBuff then
-        buffConfig.cdW = os.time() + tonumber(buffSetup[3])
+        storage.buffCDW = os.time() + tonumber(buffSetup[3])
     end
 end)
+
+checkBox.checkPZ = setupUI([[
+CheckBox
+  id: checkBox
+  font: cipsoftFont
+  text: Not Use PZ
+]])
+
+checkBox.checkPZ.onCheckChange = function(widget, checked)
+  storage.castPZ = checked
+end
+
+if storage.castPZ == nil then storage.castPZ = true end
+
+checkBox.checkPZ:setChecked(storage.castPZ)
