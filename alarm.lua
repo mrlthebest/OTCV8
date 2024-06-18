9df2853a22f2f76867581cc0ed2896de7b745cc0
mrlthebest <115442027+mrlthebest@users.noreply.github.com>
2024-06-18 08:43:41 -0300

    alarm.lua

--- alarm.lua
+++ alarm.lua
@@ -1,112 +1,117 @@
+local config = {
+    -- [[ player alarm config ]] --
+    playerScreen = false, -- se estiver true, vai apitar quando aparecer player na tela(que não for guild/party)
+    playersName = '', -- se quiser só filtrar alguns players especificos, deixe false acima e adicione os players separando-os por ","
 
-local alarmBox = {}
+    --[[ monster alarm config ]] --
+    monsterScreen = false, -- se estiver true, vai apitar quando aparecer monstro na tela
+    monstersName = '', -- se quiser só filtrar alguns monstros especificos, deixe false acima e adicione os players separando-os por ","
 
-ui = setupUI([[
+    --[[ hp/life config ]] --
+    alarmLife = false, -- se estiver true, vai apitar quando estiver X% de vida, porcentagem editavel a baixo
+    lifePercent = 100,
+    alarmMana = false, -- se estiver true, vai apitar quando estiver X% de vida, porcentagem editavel a baixo
+    manaPercent = 100,
 
-Panel
-  height: 80
+    --[[ others config ]] --
+    enemyScreen = false, -- se estiver true, vai apitar quando aparecer enemy(blue emblem) na tela
+    pkScreen = false, -- se estiver true, vai apitar quando aparecer pk na tela
+    selfPk = false, -- se estiver true, vai apitar quando vc pegar pk
+    playerAttack = false, -- se estiver true, vai apitar quando algum player te atacar(pode ocorrer bug dependendo da rev, se ocorrer não utilize a opção)
 
-  CheckBox
-    id: playerD
-    font: cipsoftFont
-    text: Player Detected
-    anchors.top: parent.top
-    anchors.left: parent.left
-    anchors.right: parent.right
-    text-align: bottom
-    text-offset: 17 0
+};
 
-  CheckBox
-    id: playerP
-    font: cipsoftFont
-    text: Player PK
-    anchors.left: parent.left
-    anchors.right: parent.right
-    anchors.top: prev.bottom
-    text-align: bottom
-    text-offset: 17 0  
+-- não edite nada abaixo
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-  CheckBox
-    id: playerH
-    font: cipsoftFont
-    text: Low Life/HP
-    anchors.left: parent.left
-    anchors.right: parent.right
-    anchors.top: prev.bottom
-    text-align: bottom
-    text-offset: 17 0  
-
-  CheckBox
-    id: playerSP
-    font: cipsoftFont
-    text: Self PK
-    anchors.left: parent.left
-    anchors.right: parent.right
-    anchors.top: prev.bottom
-    text-align: bottom
-    text-offset: 17 0  
-]])
-
----------------------------------------------------------------
-ui.playerD.onCheckChange = function(widget, checked)
-    storage.playerDetected = checked
+local function playAlarm()
+    playSound("/sounds/alarm.ogg");
+    g_window.flash();
 end
 
-if storage.playerDetected == nil then storage.playerDetected = true end
-
-ui.playerD:setChecked(storage.playerDetected)
----------------------------------------------------------------
-ui.playerP.onCheckChange = function(widget, checked)
-    storage.playerPK = checked
+if type(getCreatureByName) ~= 'function' then
+    getCreatureByName = function(name)
+        if not name then return nil; end
+        name = name:lower();
+        for i, spec in ipairs(g_map.getSpectators(player:getPosition())) do
+            if spec:getName():lower() == name then
+                return spec;
+            end
+        end
+        return nil;
+    end
 end
 
-if storage.playerPK == nil then storage.playerPK = true end
-
-ui.playerP:setChecked(storage.playerPK)
----------------------------------------------------------------
-ui.playerH.onCheckChange = function(widget, checked)
-    storage.lowLife = checked
+local function findCreatures(creatureList)
+    local toSearch = string.split(creatureList, ',');
+    for _, possibleCreature in ipairs(toSearch) do
+        if getCreatureByName(possibleCreature) then
+            return true;
+        end
+    end
+    return false;
 end
 
-if storage.lowLife == nil then storage.lowLife = true end
-
-ui.playerH:setChecked(storage.lowLife)
----------------------------------------------------------------
-ui.playerSP.onCheckChange = function(widget, checked)
-    storage.selfPK = checked
+local function hasPlayersScreen()
+    for _, spec in ipairs(getSpectators()) do
+        if spec ~= player and spec:isPlayer() and (spec:getEmblem() ~= 1 or not spec:isPartyMember()) then
+            return true;
+        end
+    end
+    return false;
 end
 
-if storage.selfPK == nil then storage.selfPK = true end
+local function hasMonstersScreen()
+    for _, spec in ipairs(getSpectators()) do
+        if spec:isMonster() then
+            return true;
+        end
+    end
+    return false;
+end
 
-ui.playerSP:setChecked(storage.selfPK)
----------------------------------------------------------------
+local function pkScreen()
+    for _, spec in ipairs(getSpectators()) do
+        if spec ~= player and spec:isPlayer() and spec:getSkull() > 2 and (spec:getEmblem() ~= 1 or not spec:isPartyMember())  then
+            return true;
+        end
+    end
+    return false;
+end
+
+local colorToMatch = {r = 0, g = 0, b = 0, a = 255};
+local function getAttacked()
+    for _, spec in ipairs(getSpectators()) do
+        if spec:isPlayer() and spec:isTimedSquareVisible() and table.equals(spec:getTimedSquareColor(), colorToMatch) then
+            return true;
+        end
+    end
+    return false;
+end
 
 
 macro(100, "Alarm", function()
-    local configAlarm = storage.alarmX:split(",")
-    local playerPos = player:getPosition();
+    local alarmPlayersScreen = config.playerScreen;
+    local playersList = config.playersName;
+    local alarmMonstersScreen = config.monsterScreen;
+    local monsterList = config.monstersName;
+    local alarmPkScreen = config.pkScreen;
+    local alarmSelfPk = config.selfPk;
+    local alarmLife, lifePercent = config.alarmLife, config.lifePercent;
+    local alarmMana, manaPercent = config.alarmMana, config.manaPercent;
+    local alarmAttacked = config.playerAttack;
     local selfHealth, selfMana = hppercent(), manapercent();
-    local selfSkull = player:getSkull();
-    for _, spec in ipairs(getSpectators()) do
-        if spec:isPlayer() then
-            local specPos = spec:getPosition();
-            local specSkull = spec:getSkull();
-            local distanceToSpec = getDistanceBetween(playerPos, specPos);
-            if (
-                (spec ~= player and distanceToSpec < 8 and storage.playerDetected) or
-                (spec ~= player and specSkull > 2 and storage.playerPK) or
-                (selfSkull > 2 and storage.selfPK) or
-                ((selfHealth <= tonumber(configAlarm[1]) or selfMana <= tonumber(configAlarm[2])) and storage.lowLife)
-            ) then
-                if not x or x <= os.time() then
-                    playSound("/sounds/alarm.ogg")
-                    x = os.time() + 4;
-                end
-            end
-        end
+    if isInPz() then return; end
+    if (
+        ((alarmPlayersScreen and hasPlayersScreen()) or (not alarmPlayersScreen and #playersList ~= 0 and findCreatures(playersList))) or
+        ((alarmMonstersScreen and hasMonstersScreen()) or (not alarmMonstersScreen and #monsterList ~= 0 and findCreatures(monsterList))) or
+        (alarmPkScreen and pkScreen()) or
+        (alarmSelfPk and player:getSkull() >= 3) or
+        (alarmLife and selfHealth <= lifePercent) or
+        (alarmMana and selfMana <= manaPercent) or
+        (alarmAttacked and getAttacked())
+        ) then
+        playAlarm();
+        delay(6000);
     end
-end);
-
-addTextEdit("HP, MP", storage.alarmX or "HP, MP", function(widget, text)
-    storage.alarmX = text;
 end);
