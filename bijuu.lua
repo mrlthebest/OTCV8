bcde2c846be1312e7c0c0e49dd84e48509c0bc3c
mrlthebest <115442027+mrlthebest@users.noreply.github.com>
2024-06-18 09:00:31 -0300

    bijuu.lua

---
+++ bijuu.lua
@@ -0,0 +1,27 @@
+local bijuuOutfit = 23; -- id da bijuu
+
+-- n altere abaixo
+local bijuuSpells = {
+    {spell = 'bijuu fuujin', cooldown = 5},
+    {spell = 'bijuu yaiba', cooldown = 10}
+};
+
+macro(100, "Bijuu Attack", function()
+    if player:getOutfit().type ~= bijuuOutfit then return; end
+    for key, value in ipairs(bijuuSpells) do
+        if (not value.cooldownSpells or value.cooldownSpells <= os.time()) then
+            say(value.spell)
+        end
+    end
+end);
+
+onTalk(function(name, level, mode, text, channelId, pos)
+    if name ~= player:getName() then return; end
+    text = text:lower();
+    for key, value in ipairs(bijuuSpells) do
+        if text == value.spell then
+            value.cooldownSpells = os.time() + value.cooldown;
+            break;
+        end
+    end
+end);
