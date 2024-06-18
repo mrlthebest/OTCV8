ae1a5f81f65d76e30917807587a16ba2af20f002
mrlthebest <115442027+mrlthebest@users.noreply.github.com>
2024-06-18 08:57:25 -0300

    potion.lua

--- potion.lua
+++ potion.lua
@@ -1,28 +1,41 @@
---[[
-    Script de Potion
-    by mrlthebest.
-    28/07/2023
-]]--
+local configPotion = {
+    idPotion = 11862,
+    percentHp = 100,
+    percentMana = 99, 
+    cooldownPotion = 5000, -- em milisegundos
+    hidePotionText = false, -- se estiver true, vai apagar a mensagem que está usando a senzu na tela, se for false, não.
+    senzuName = 'senzu bean', -- nome da potion
+    possibleTexts = { -- não altere
+        'Aaahhh! Bem Melhor!', 
+        'Aaahhh!', 
+        'Bem Melhor!'
+    },
+}
 
-local s = {};
-
-macro(100, "Potion", function()
-    local configPotion = storage.potion:split(",");
-    if (hppercent() <= tonumber(configPotion[2]) or manapercent() <= tonumber(configPotion[3])) and (not s.cdW or s.cdW <= now) then
-        useWith(tonumber(configPotion[1]), player);
+macro(100, "Senzu", function()
+    local selfHealth, selfMana = hppercent(), manapercent();
+    if (selfHealth <= configPotion.percentHp or selfMana <= configPotion.percentMana) then
+        if (not configPotion.cooldownUse or configPotion.cooldownUse <= now) then
+            useWith(configPotion.idPotion, player)
+        end
     end
 end);
 
-onUseWith(function(pos, itemId, target, subType)
-    local configPotion = storage.potion:split(",");
-    if itemId == tonumber(configPotion[1]) then
-        s.cdW = now + tonumber(configPotion[4]);
+onTextMessage(function(mode, text)
+    if not configPotion.hidePotionText then return; end
+    if text:lower():find(configPotion.senzuName:lower()) then
+        modules.game_textmessage.clearMessages();
     end
 end);
 
-addTextEdit("Config Potion", storage.potion or "ID, HP, MP, CD", function(widget, text)
-    if text and #text:split(",") < 4 then
-        return warn("por favor, inserir os valores na ordem (ID, HP, MP, CD)");
+onTalk(function(name, level, mode, text, channelId, pos)
+    if (player:getName() ~= name) then return; end
+    text = text:lower();
+    for _, possibleText in ipairs(configPotion.possibleTexts) do
+        filterText = possibleText:trim():lower();
+        if text:find(filterText) then
+            configPotion.cooldownUse = now + configPotion.cooldownPotion;
+            break;
+        end
     end
-    storage.potion = text;
 end);
