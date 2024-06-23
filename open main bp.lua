f1e7e4508d91612e0bb81f71417f0a9a382f04d1
mrlthebest <115442027+mrlthebest@users.noreply.github.com>
2024-06-23 01:11:24 -0300

    Add files via upload

---
+++ open main bp.lua
@@ -0,0 +1,11 @@
+macro(200, "Keep Open Main BP", function()
+    --Made By VivoDibra#1182
+    if not getContainers()[0] and getBack() then
+        g_game.open(getBack())
+    end
+end)
+
+--Join Discord server for free scripts
+--https://discord.gg/RkQ9nyPMBH
+--Made By VivoDibra#1182
+--Tested on vBot 4.8 / OTCV8 3.1 rev 232
